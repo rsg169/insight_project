5533c7513c7493876906548b298e7a8164e3776c
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2020-02-08 05:18:56 +0000

    07022020 end of day commit

--- data-processing/run.py
+++ data-processing/run.py
@@ -3,33 +3,36 @@ import os
 from pyspark import SparkContext, SparkConf
 from indexes import getIndexes
 
-# constants
-submit_str = '$SPARK_HOME/bin/spark-submit --py-files sparkcc.py --jars ~/postgresql-42.2.9.jar --master spark://ec2-44-226-24-33.us-west-2.compute.amazonaws.com:7077 ./word_count.py hdfs://ec2-44-226-24-33.us-west-2.compute.amazonaws.com:9000/'
-#cleanup_str = 'hdfs dfs -rm -R /input'
-
-# configuration to read from s3
-conf = SparkConf().setAppName('ingestion')
+# Configuration to read from S3
+conf = SparkConf().setAppName('run')
 sc = SparkContext(conf=conf)
 sc._jsc.hadoopConfiguration().set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
-# open files and write headers
+# Begin writing to the submit and cleanup files
 submit_file = open('submit.sh','w+')
+submit_file.write('#!/bin/sh\n')
+submit_file.write('start=$(date +%s)\n')
+
 cleanup_file = open('cleanup.sh','w+')
-submit_file.write('#!/bin/sh'+'\n')
-submit_file.write('start=`date +%s`'+'\n')
-cleanup_file.write('#!/bin/sh'+'\n')
-cleanup_file.write('rm submit.sh'+'\n')
-cleanup_file.write('rm -rf input'+'\n')
-cleanup_file.write('hdfs dfs -rm -r /input'+'\n')
+cleanup_file.write('#!/bin/sh\n')
+cleanup_file.write('rm submit.sh\n')
+cleanup_file.write('rm -rf input\n')
+cleanup_file.write('hdfs dfs -rm -r /input\n')
+cleanup_file.write('rm -rf spark-warehouse\n')
 
+# Create the necessary directories locally and on HDFS
+os.makedirs(os.getcwd()+'/input', exist_ok=True)
 os.popen('hdfs dfs -mkdir /input')
-#cleanup_file.write('rm -rf input/temp'+'\n')
 
-# prompt user for input
+# Prompt the user for input
+pdns = input("Enter the Public DNS of this cluster's master node: ")
 start = input("Enter start month and year (e.g. January 2019): ")
 end = input("Enter end month and year: ")
 
-# retrieve a list of paths to index files
+# Construct the submit string
+submit_str = '$SPARK_HOME/bin/spark-submit --py-files sparkcc.py --jars ~/postgresql-42.2.9.jar --master spark://'+pdns+':7077 ./word_count.py hdfs://'+pdns+':9000/'
+
+# Retrieve a list of paths to index files
 indexList = getIndexes(start,end)
 
 # Loop to read the index files and create input files from their content
@@ -44,21 +47,25 @@ for index in indexList:
             filename = 'input/'+time+'_wet.txt'
             input_file = open(filename,'a+')
             input_file.write('s3://commoncrawl/'+archive+'\n')
-#           os.popen('hdfs dfs -copyFromLocal '+filename+' /'+filename)
             submit_file.write(submit_str+filename+' o'+time+'\n')
-#           cleanup_file.write(cleanup_str+'o'+time+'\n')
         #input_file.write('s3://commoncrawl/'+archive+'\n')
 
-input_file.close()
-submit_file.write('end=`date +%s`'+'\n')
-submit_file.write('runtime=$((end-start))'+'\n')
-submit_file.write('echo $runtime'+'\n')
-submit_file.close()
+# Additional commands to record the time of the job
+submit_file.write('end=$(date +%s)\n')
+submit_file.write('runtime=$((end-start))\n')
+submit_file.write('echo "Execution time was $runtime seconds"\n')
+
+# Copy the input files to HDFS
 os.popen('hdfs dfs -copyFromLocal $PWD/input /')
+
+# Close the open file objects
+input_file.close()
+submit_file.close()
 cleanup_file.close()
 
+# Change permissions on the submit and cleanup files to make them executable
 os.popen('chmod 755 submit.sh')
 os.popen('chmod 755 cleanup.sh')
 
-# Terminal command to execute the job
+# Execute the job
 #os.popen('./submit.sh')
