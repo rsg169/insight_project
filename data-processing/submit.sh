548613c59b5dbc58227bd2c0c3f4db8b8cb02b6d
rsg169 <56736836+rsg169@users.noreply.github.com>
2020-02-07 18:12:58 -0800

    Delete submit.sh

--- data-processing/submit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-$SPARK_HOME/bin/spark-submit --py-files sparkcc.py,process_output.py --driver-class-path ~/postgresql-42.2.9.jar --master spark://44.231.205.255:7077 ./word_count.py hdfs://44.231.205.255:9000/user/20170322212946_wet.txt o20170322212946
-#$SPARK_HOME/bin/spark-submit --driver-class-path ~/postgresql-42.2.9.jar --master local[*] ./word_count.py ./input/20170322212946_wet.txt o20170322212946
-#$SPARK_HOME/bin/spark-submit --driver-class-path ~/postgresql-42.2.9.jar --master local[*] ./word_count.py ./input/20170322212947_wet.txt o20170322212947
